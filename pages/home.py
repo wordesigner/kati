f8587b1fb5acbe50d000cce2735354191bddf22c
wordesigner <116449904+wordesigner@users.noreply.github.com>
2024-03-11 00:52:06 +0330

    Add files via upload

--- pages/home.py
+++ pages/home.py
@@ -15,25 +15,51 @@ layout = html.Div([
     dbc.Container([
         dbc.Row([
             dbc.Col([
-                html.Div(html.I(className='bi bi-circle-fill'), style={'font-size':'100px'})
-            ], className='col-md-3 col-sm-6'),
+                html.Div(html.I(className='bi bi-card-list'), style={'font-size':'200px'}),
+            ], className='col-md-4 col-sm-6', style={'text-align':'left'}),
             dbc.Col([
-                html.Div(html.I(className='bi bi-circle-fill'), style={'font-size':'100px'})
-            ], className='col-md-3 col-sm-6')
+                
+            ], className='col-md-2', style={'text-align':'center'}),
+            dbc.Col([
+                
+            ], className='col-md-2', style={'text-align':'center'}),
+            dbc.Col([
+                html.Div(html.I(className='bi bi-search'), style={'font-size':'200px'}),
+            ], className='col-md-4 col-sm-6')
         ]),
     ]),
     dbc.Container([
         dbc.Row([
             dbc.Col([
                 html.Div([
-                    dbc.Carousel(
-                        items=[
-                            {'key':'1', 'src':'https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/spectrum.jpg'},
-
-                        ], ride='carousel', variant='dark'
-                    )
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/1.jpg', width='100%', height='250px'),
                 ], className='bg-primary')
-            ], style={'padding-right':'0px'})
-        ])
+            ], style={'padding-right':'0px'}, md=2, sm=2),
+            dbc.Col([
+                html.Div([
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/3.jpg', width='100%', height='250px'),
+                ], className='bg-primary')
+            ], style={'padding-right':'0px'}, md=2, sm=2, className='element'),
+            dbc.Col([
+                html.Div([
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/4.jpg', width='100%', height='250px'),
+                ], className='bg-primary')
+            ], style={'padding-right':'0px'}, md=2, sm=2, className='element'),
+            dbc.Col([
+                html.Div([
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/1.jpg', width='100%', height='250px'),
+                ], className='bg-primary')
+            ], style={'padding-right':'0px'}, md=2, sm=2, className='element'),
+            dbc.Col([
+                html.Div([
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/3.jpg', width='100%', height='250px'),
+                ], className='bg-primary')
+            ], style={'padding-right':'0px'}, md=2, sm=2, className='element'),
+            dbc.Col([
+                html.Div([
+                    html.Img(src='https://www.alirezaoboodiat.com/wp-content/uploads/2024/03/4.jpg', width='100%', height='250px'),
+                ], className='bg-primary')
+            ], style={'padding-right':'0px'}, md=2, sm=2, className='element'),
+        ], style={'margin-top':'132px'})
     ], fluid=True, className='g-0')
 ])
