ec9e875f15d27aab1f68b858cfa3c739318289a5
wordesigner <116449904+wordesigner@users.noreply.github.com>
2024-03-12 15:33:43 +0330

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -16,12 +16,12 @@ layout = html.Div([
         dbc.Row([
             
             dbc.Col([
-                html.Div(html.I(className='bi bi-card-list'), style={'font-size':'150px'}),
+                html.A(html.I(className='bi bi-card-list'), style={'font-size':'150px'}, href='/browse', target='_self', id='persian-green'),
                 dbc.Col([
                     
                 ], className='col'),
                 dbc.Col([
-                    html.A('فهرست ابزارها', href='/browse', target='_self')
+                    html.A('فهرست ابزارها', href='/browse', target='_self', id='persian-black')
                 ], className='col main-page-services'),
                 dbc.Col([
                     
@@ -34,12 +34,12 @@ layout = html.Div([
                 
             ], className='col', style={'text-align':'center'}),
             dbc.Col([
-                html.Div(html.I(className='bi bi-search'), style={'font-size':'150px'}),
+                html.A(html.I(className='bi bi-search'), style={'font-size':'150px'}, href='/search', target='_self', id='persian-green'),
                 dbc.Col([
                     
                 ], className='col' ,style={'text-align':'center'}),
                 dbc.Col([
-                    html.A('جستجو', href='/search', target='_self')
+                    html.A('جستجو', href='/search', target='_self', id='persian-black')
                 ], className='col main-page-services' ,style={'text-align':'center'}),
                 dbc.Col([
                     
